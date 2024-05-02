9ecdcb428010b02f0b21b1beb6894d6ded74300e
WonChanLee <67488930+WonChanLee@users.noreply.github.com>
2024-05-02 17:53:09 -0400

    Airbridge_final ver

--- cpw/CPWStraightAirbridge.py
+++ cpw/CPWStraightAirbridge.py
@@ -12,18 +12,19 @@ class CPWStraight_AB(Component):
     _defaults['length'] = 100
     _defaults['pinw'] = 20
     _defaults['gapw'] = 8.372
-    _defaults['ABwidth'] = 80
+    _defaults['ABwidth'] = 40
     _defaults['ABheight'] = 40
-    _defaults['ABlength'] = 50
+    _defaults['ABlength'] = 60 # or 50
     _defaults['ABgap'] = 200
+    _defaults['ABstep2gap'] = 5
     
     def __init__(self,structure,startjunc=None,settings={}, cxns_names = ['in','out']):
         """ Adds a straight section of CPW transmission line of length = length to the structure"""        
         s=structure
         
         comp_key = 'CPWStraight'
-        global_keys = ['pinw','gapw','ABwidth','ABheight','ABlength']
-        object_keys = ['pinw','gapw','ABwidth','ABheight','ABlength'] # which correspond to the extract global_keys
+        global_keys = ['pinw','gapw','ABwidth','ABheight','ABlength','ABgap','ABstep2gap']
+        object_keys = ['pinw','gapw','ABwidth','ABheight','ABlength','ABgap','ABstep2gap'] # which correspond to the extract global_keys
         Component.__init__(self,structure,comp_key,global_keys,object_keys,settings)
         settings = self.settings
         
@@ -34,6 +35,7 @@ class CPWStraight_AB(Component):
         ABheight = settings['ABheight']
         ABlength = settings['ABlength']
         ABgap = settings['ABgap']
+        ABstep2gap = settings['ABstep2gap']
                 
         if startjunc is None: startjunc=s.last.copyjunc()
         
@@ -64,6 +66,9 @@ class CPWStraight_AB(Component):
             iter_num = ABnum-1
 
         if 200 <= length:
+        # ABbox1 : top abstep1
+        # ABbox2 : bottom abstep1
+        # ABbox3 : abstep2
              ABbox1 = []
              ABbox2 = []
              ABbox3 = []
@@ -77,11 +82,11 @@ class CPWStraight_AB(Component):
                             (coords[0]+length/2+ABwidth/2,coords[1]-ABlength/2-ABheight), 
                             (coords[0]+length/2-ABwidth/2,coords[1]-ABlength/2-ABheight), 
                             (coords[0]+length/2-ABwidth/2,coords[1]-ABlength/2)])
-             ABbox3.append([(coords[0]+length/2-ABwidth/2,coords[1]+ABlength/2+ABheight), 
-                            (coords[0]+length/2+ABwidth/2,coords[1]+ABlength/2+ABheight), 
-                            (coords[0]+length/2+ABwidth/2,coords[1]-ABlength/2-ABheight), 
-                            (coords[0]+length/2-ABwidth/2,coords[1]-ABlength/2-ABheight), 
-                            (coords[0]+length/2-ABwidth/2,coords[1]+ABlength/2+ABheight)])
+             ABbox3.append([(coords[0]+length/2-ABwidth/2-ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap), 
+                            (coords[0]+length/2+ABwidth/2+ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap), 
+                            (coords[0]+length/2+ABwidth/2+ABstep2gap,coords[1]-ABlength/2-ABheight-ABstep2gap), 
+                            (coords[0]+length/2-ABwidth/2-ABstep2gap,coords[1]-ABlength/2-ABheight-ABstep2gap), 
+                            (coords[0]+length/2-ABwidth/2-ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap)])
              #right ABbox
              for i in range(iter_num):
                 ABbox1.append([(coords[0]+length/2-ABwidth/2+ABgap*(i+1),coords[1]+ABlength/2), 
@@ -94,11 +99,11 @@ class CPWStraight_AB(Component):
                                (coords[0]+length/2+ABwidth/2+ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
                                (coords[0]+length/2-ABwidth/2+ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
                                (coords[0]+length/2-ABwidth/2+ABgap*(i+1),coords[1]-ABlength/2)])
-                ABbox3.append([(coords[0]+length/2-ABwidth/2+ABgap*(i+1),coords[1]+ABlength/2+ABheight), 
-                               (coords[0]+length/2+ABwidth/2+ABgap*(i+1),coords[1]+ABlength/2+ABheight), 
-                               (coords[0]+length/2+ABwidth/2+ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
-                               (coords[0]+length/2-ABwidth/2+ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
-                               (coords[0]+length/2-ABwidth/2+ABgap*(i+1),coords[1]+ABlength/2+ABheight)])
+                ABbox3.append([(coords[0]+length/2-ABwidth/2+ABgap*(i+1)-ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap), 
+                               (coords[0]+length/2+ABwidth/2+ABgap*(i+1)+ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap), 
+                               (coords[0]+length/2+ABwidth/2+ABgap*(i+1)+ABstep2gap,coords[1]-ABlength/2-ABheight-ABstep2gap), 
+                               (coords[0]+length/2-ABwidth/2+ABgap*(i+1)-ABstep2gap,coords[1]-ABlength/2-ABheight-ABstep2gap), 
+                               (coords[0]+length/2-ABwidth/2+ABgap*(i+1)-ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap)])
              # left ABbox
              for i in range(iter_num):
                 ABbox1.append([(coords[0]+length/2-ABwidth/2-ABgap*(i+1),coords[1]+ABlength/2), 
@@ -111,11 +116,11 @@ class CPWStraight_AB(Component):
                                (coords[0]+length/2+ABwidth/2-ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
                                (coords[0]+length/2-ABwidth/2-ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
                                (coords[0]+length/2-ABwidth/2-ABgap*(i+1),coords[1]-ABlength/2)])
-                ABbox3.append([(coords[0]+length/2-ABwidth/2-ABgap*(i+1),coords[1]+ABlength/2+ABheight), 
-                               (coords[0]+length/2+ABwidth/2-ABgap*(i+1),coords[1]+ABlength/2+ABheight), 
-                               (coords[0]+length/2+ABwidth/2-ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
-                               (coords[0]+length/2-ABwidth/2-ABgap*(i+1),coords[1]-ABlength/2-ABheight), 
-                               (coords[0]+length/2-ABwidth/2-ABgap*(i+1),coords[1]+ABlength/2+ABheight)])
+                ABbox3.append([(coords[0]+length/2-ABwidth/2-ABgap*(i+1)-ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap), 
+                               (coords[0]+length/2+ABwidth/2-ABgap*(i+1)+ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap), 
+                               (coords[0]+length/2+ABwidth/2-ABgap*(i+1)+ABstep2gap,coords[1]-ABlength/2-ABheight-ABstep2gap), 
+                               (coords[0]+length/2-ABwidth/2-ABgap*(i+1)-ABstep2gap,coords[1]-ABlength/2-ABheight-ABstep2gap), 
+                               (coords[0]+length/2-ABwidth/2-ABgap*(i+1)-ABstep2gap,coords[1]+ABlength/2+ABheight+ABstep2gap)])
              for i in range(2*iter_num+1):
                 ABbox1[i] = rotate_pts(ABbox1[i],startjunc.direction,coords)
                 ABbox2[i] = rotate_pts(ABbox2[i],startjunc.direction,coords)
